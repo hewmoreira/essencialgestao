861e402faefea4da6b77f62e1de6face6c605aca
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-09-18 17:56:04 -0300

    essencialgestao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultarComponent } from './consultar/consultar.component';
+import { LoginComponent } from './login/login.component';
+import { NewuserComponent } from './newuser/newuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'newuser', component: NewuserComponent},
+  {path: 'consultar', component: ConsultarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewuserComponent } from './newuser/newuser.component';
+import { LoginComponent } from './login/login.component';
+import { ConsultarComponent } from './consultar/consultar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewuserComponent,
+    LoginComponent,
+    ConsultarComponent
   ],
   imports: [
     BrowserModule,
